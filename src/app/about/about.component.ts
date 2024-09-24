7f8737aaeac1d92e32b7bb549728c93fd6c0897f
falda <54033784+falda@users.noreply.github.com>
2024-09-24 20:02:34 +0200

    J'ai finis la page about, il ne me reste plus que la page projects à faire.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  downloadCV(): void {
+    const link = document.createElement('a');
+    link.href = '/myCV.pdf'; 
+    link.download = 'myCV.pdf'; 
+    link.click();
+  }
 }
